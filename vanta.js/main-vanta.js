3986d85bb8426eccc1006718b4c0347b78ae16ec
svnggggg <elperuco784@gmail.com>
2024-07-04 21:24:20 -0300

    Comenzando el menu de skills

---
+++ vanta.js/main-vanta.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.FOG({
+        el: ".body",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        highlightColor: 0x0,
+        midtoneColor: 0x151557,
+        lowlightColor: 0x0,
+        baseColor: 0x163664,
+        blurFactor: 0.23,
+        speed: 0.40,
+        zoom: 0.40
+    })
+});
